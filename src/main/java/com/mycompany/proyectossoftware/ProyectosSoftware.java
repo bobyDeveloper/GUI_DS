660d74c5b5241575ac8114319db612dbd8e87eb3
bobyDeveloper <zS21004509@estudiantes.uv.mx>
2024-09-08 01:04:30 -0600

    GUI

---
+++ src/main/java/com/mycompany/proyectossoftware/ProyectosSoftware.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectossoftware;
+
+/**
+ *
+ * @author Ville
+ */
+public class ProyectosSoftware {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
